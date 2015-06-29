7d6b06561a5aa394eae4f539e81f7efbda414bfe
BuildTools <unconfigured@null.spigotmc.org>
2015-06-29 17:25:39 +0000

    Updates

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
+// passport for login
 var passport = require('passport');
 var passportlocal = require('passport-local');
 var passporthttp = require('passport-http');
@@ -17,9 +18,21 @@ var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-var bcrypt = require('bcrypt');
+// awaiting solve
+// var bcrypt = require('bcrypt');
 
 var app = express();
+
+// sign in schema
+
+var userSchema = new mongoose.Schema({
+  username: { type: String }
+, email: String
+, pwd: String
+});
+var exits = false;
+var user = mongoose.model('user', userSchema);
+
 
 // custom scripts
 // var mod = require('./module');
@@ -61,27 +74,59 @@ app.get('/signup', function (req, res) {
 	res.render('signupREAL.ejs');
 });
 
-app.listen("8080", function () {
-	console.log("Running on port 8080");
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+	console.log("Running on port " + port);
 	console.log("The date and time is:", Date());
     connect("ok");
 } );
 
+
 app.post('/signup', function (req, res) {
 	//hash the password
-    var username = res.body.username;
-    var email = res.body.email;
-    var pass;
-    bcrypt.genSalt(10, function(err, salt) {
-    pass = bcrypt.hash(res.body.pass, salt, function(err, hash) {
-        // Store hash in your password DB. 
-    });
-    db.users.save({"username": username, "email": email, "password": pass});
+    var susername = req.body.username;
+    var semail = req.body.email;
+    var pass = req.body.pass;
+    
+    var signupuser = new user({
+  username: susername
+, email: semail
+, pwd: pass
 });
+
+
+user.findOne({ username: susername }, function(err, signupuser) {
+  if (!err){
+      console.log("User tried to signup taken username");
+      exits = true;
+      res.render("signupREAL.ejs");
+  };
+});
+
+
+
+signupuser.save(function(err, signupuser) {
+  if (err){
+      return console.error("Errors storing DATA: " + err );
+  } else {
+      console.log("Done");
+      res.render('signin.ejs');
+  } 
+});
+
+    // later
+    /// bcrypt.genSalt(10, function(err, salt) {
+    // for later pass = bcrypt.hash(res.body.pass, salt, function(err, hash) {
+        // Store hash in your password DB. 
+    // });
+    // });
+    // db.collection('users').insertOne({ "username": susername, "email": semail, "password": pass });
+
     console.log("Sign Up succesful");
     console.log("Storing...");
     
-    res.render('signin.ejs');
+    
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
