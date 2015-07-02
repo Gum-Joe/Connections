a35130a2d1ef61b1438a0e750bb1474094d0b087
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-07-02 19:40:36 +0100

    Fixes

--- test/databasetest.js
+++ test/databasetest.js
@@ -30,7 +30,7 @@ MongoClient.connect(url, function(err, db) {
 
 describe('Database tests', function () {
 	it('Can we connect to the database?', function (done) {
-		exports.connect = function () {
+		var connect = function () {
 	mongoose.connect('mongodb://localhost:27017/connections');
 
 db = mongoose.connection;
@@ -40,11 +40,19 @@ db.once('open', function (callback) {
   console.log("Yay! We succefully connected to the db");
 });
 }
+connect();
 done();
 	});
 	
 	it('Can we connect Store Data?', function (done) {
-		/*
+    var testSchema = new mongoose.Schema({
+  name: { type: String }
+, date: String
+, testtype: String
+});
+var exits = false;
+var test = mongoose.model('test', testSchema);
+
 	mongoose.connect('mongodb://localhost:27017/connections');
 
 db = mongoose.connection;
@@ -52,12 +60,57 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
   // yay!
   console.log("Yay! We succefully connected to the db");
+});
+var name = "test-" + Date();
+    var date = Date();
+    var typet = "git-PC-" + Date();
+
+        var signupuser = new test({
+  name: name
+, date: date
+, testtype: typet
+});
+
+/*
+user.findOne({ username: susername }, function(err, signupuser) {
+  if (!err){
+      console.log("User tried to signup taken username");
+      exits = true;
+      res.render("signupREAL.ejs");
+  };
+});
 */
 
 
-done();
+
+signupuser.save(function(err, signupuser) {
+  if (err){
+      return console.error("Errors storing DATA: " + err );
+      throw Error;
+  } else {
+      console.log("Done");
+      
+  } 
+    });
+    
+    // later
+    /// bcrypt.genSalt(10, function(err, salt) {
+    // for later pass = bcrypt.hash(res.body.pass, salt, function(err, hash) {
+        // Store hash in your password DB. 
+    // });
+    // });
+    // db.collection('users').insertOne({ "username": susername, "email": semail, "password": pass });
+
+    console.log("Sign Up succesful");
+    console.log("Storing...");
+    done();
+});
+
+
+
+
 	});
 	
 	// store data
  
-})
+
