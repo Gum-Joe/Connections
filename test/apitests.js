237a647e74350d9d369270cce33d37f6e6544df9
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-07-04 09:15:30 +0100

    Update apitests.js
    Add address field

--- test/apitests.js
+++ test/apitests.js
@@ -2,11 +2,12 @@ var request = require('supertest')
   , app     = require('../app.js').app
   , assert  = require("assert");
 
-describe('GET tests', function(){
+describe('GET and POST tests', function(){
   it('Can we run GET requests?', function(done){
     request(app)
         .get('/')
         .expect(300)
+        .address("http://localhost:8080")
         .end(function(err, res){
             done();
         })
