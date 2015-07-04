921c069799e6e39ebf2c01d5a72e571edac5103c
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-07-04 09:37:37 +0100

    Update apitests.js
    Add sign up

--- test/apitests.js
+++ test/apitests.js
@@ -2,10 +2,12 @@ var request = require('supertest')
   , app     = require('../app.js').app
   , assert  = require("assert");
 
+var address = "localhost:8080";
+
 describe('GET and POST tests', function(){
   it('Can we run GET requests?', function(done){
     request(app)
-        .get('http://localhost:8080/')
+        .get('/signup')
         .expect(300)
         .address("http://localhost:8080")
         .end(function(err, res){
